17cfe7843e95393e5cac824c99fa92d152a67407
robynainsley21 <robynainsley2i@gmail.com>
2023-06-01 14:28:15 +0200

    Solved challenge

--- DWA-5/scripts.js
+++ DWA-5/scripts.js
@@ -1,10 +1,25 @@
-
+// Document elements
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
+const screen = document.querySelector("html");
 
+// Errors that will run when the conditionals result in true
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
+  event.preventDefault(); 
+
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
-});
+  
+  if (dividend === '' && divider === '') {
+    result.innerText = 'Division not performed. Both values are required in inputs. Try again';
+  } else if (divider < 0) {
+    result.innerText = 'Division not performed. Invalid number provided. Try again';
+    throw new Error ('Division not performed. Invalid number provided. Try again');
+  } else if (dividend !== isNaN && divider !== isNaN) {
+    screen.innerHTML = 'Something critical went wrong. Please reload the page';
+    throw new Error ('Something critical went wrong. Please reload the page');
+  } else {
+    result.innerText = Math.floor(dividend / divider);
+  };
+  
+}); 
